52e20beb6daf1964324a7bc437e5c61c745b2c19
IS95890 <umit.Yildirim@softtech.isbank.com.tr>
2022-11-25 16:39:11 +0300

    first commit

---
+++ Controllers/TeaController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TeaApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TeaController : ControllerBase
+    {
+        private static readonly string[] TeaType = new[]
+       {
+            "Kara Çay", "Ihlamur"
+        };
+
+        [HttpGet]
+        public ActionResult Get()
+        {
+            Request.Headers.ToList().ForEach(x =>
+                Console.WriteLine(x.Key + " " + x.Value));
+
+            Console.WriteLine("Host: " + Request.Host);
+
+            Console.WriteLine("Path: " + Request.Path);
+            
+
+            Console.WriteLine("Çalıştı");
+
+            return Ok(TeaType[Random.Shared.Next(TeaType.Length)]);
+        }
+    }
+}
